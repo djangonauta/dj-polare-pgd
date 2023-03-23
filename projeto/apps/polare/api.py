8ba720fda6cf7c589fc5f25ecc7927196e3752bf
igor <igor.carvalho@ifpa.edu.br>
2023-03-23 11:17:45 -0300

    feat: adicionado metadata para exibição na api browsable do DRF

--- projeto/apps/polare/api.py
+++ projeto/apps/polare/api.py
@@ -4,8 +4,12 @@ from . import models, serializers
 
 
 class PlanoIndividualViewSet(viewsets.ReadOnlyModelViewSet):
+    """Lista de Planos Individuais ativos no sistema Polare"""
 
     permission_classes = [permissions.IsAdminUser]
     serializer_class = serializers.PlanoIndividualSerializer
     queryset = models.PlanoIndividual.objects.planos_para_api()
     search_fields = ('nome', 'siape', 'modelo_trabalho')
+
+    def get_view_name(self):
+        return 'Listagem de Planos Individuais'

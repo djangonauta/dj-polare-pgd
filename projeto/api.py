43d78d55a11af53d16c0078935db5f0ce79664bd
igor <igor.carvalho@ifpa.edu.br>
2023-03-30 13:05:12 -0300

    fix: corrigido basename

--- projeto/api.py
+++ projeto/api.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from projeto.apps.polare import api as polare_api
 
 router = routers.DefaultRouter()
-router.register('plano-individual', polare_api.PlanoIndividualViewSet)
+router.register('plano-individual', polare_api.PlanoIndividualViewSet, basename='plano-individual')
 router.register('plano-individual-progep', polare_api.PlanoIndividualPROGEPViewSet)
 
 urls = router.urls, 'projeto', 'v1'
